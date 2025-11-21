e04f7c89303a4da246db497f71797628c66172f6
Alex <pellieralex@gmail.com>
2025-11-21 15:42:40 +0100

    fix: add config for next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
+import type { NextConfig } from 'next';
+
+// Par défaut, ce plugin cherche le fichier dans './src/i18n/request.ts'
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Vos autres options de config ici si nécessaire */
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
