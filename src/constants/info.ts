50f0cbdfc8c173bfb82baecfb0c031686676ea73
Alex <pellieralex@gmail.com>
2025-12-08 17:58:22 +0100

    create info.ts constants

---
+++ src/constants/info.ts
@@ -0,0 +1,34 @@
+export const PHONE_NUMBER = "+32 4 79 07 92 17";
+export const EMAIL = "cafecostermans@gmail.com";
+
+export const ADDRESS = {
+    fr: "5 Place du Grand Sablon, 1000 Bruxelles",
+    nl: "Grote Zavel 5, 1000 Brussel",
+    en: "5 Place du Grand Sablon, 1000 Brussels", // Using French/International format for EN usually fine, or same as FR
+};
+
+// Start line for address components if needed separately
+export const ADDRESS_STREET = {
+    fr: "5 Place du Grand Sablon",
+    nl: "Grote Zavel 5",
+    en: "5 Place du Grand Sablon",
+};
+
+export const ADDRESS_CITY = {
+    fr: "1000 Bruxelles",
+    nl: "1000 Brussel",
+    en: "1000 Brussels",
+};
+
+export const ENTERPRISE_NUMBER = "BE1016.165.466";
+export const VAT_NUMBER = "BE1016.165.466";
+
+export const OPENING_HOURS = {
+    tuesday: "09h-19h",
+    wednesday: "09h-19h",
+    thursday: "09h-23h",
+    friday: "09h-23h",
+    saturday: "09h-23h",
+    sunday: "09h-19h",
+    monday: "Closed",
+};
